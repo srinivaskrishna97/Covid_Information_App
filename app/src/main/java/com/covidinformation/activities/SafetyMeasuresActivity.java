2c25acee6b170a710a4509eee16466e9f49cc236
parsh <parshant13rawat@gmail.com>
2021-02-05 12:36:02 -0500

    SafetyMeasuresActivity activity is created

---
+++ app/src/main/java/com/covidinformation/activities/SafetyMeasuresActivity.java
@@ -0,0 +1,26 @@
+package com.covidinformation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.covidinformation.R;
+import com.covidinformation.adapters.SafetyMeasuresAdapter;
+import com.covidinformation.models.SafetyGuidencePojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SafetyMeasuresActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety_measures);
+
+
+    }
+}
