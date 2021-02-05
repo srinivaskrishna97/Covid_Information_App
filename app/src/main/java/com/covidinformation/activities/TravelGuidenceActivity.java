44007ad942f4f142f9ac16d0f6f3866e4ba13604
parsh <parshant13rawat@gmail.com>
2021-02-05 12:42:26 -0500

    TravelGuidenceActivity activity is created

---
+++ app/src/main/java/com/covidinformation/activities/TravelGuidenceActivity.java
@@ -0,0 +1,26 @@
+package com.covidinformation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.covidinformation.R;
+import com.covidinformation.adapters.SafetyMeasuresAdapter;
+import com.covidinformation.models.SafetyGuidencePojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TravelGuidenceActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_guidence);
+
+
+    }
+}
