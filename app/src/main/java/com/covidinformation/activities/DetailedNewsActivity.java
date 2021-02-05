4c9d01a812e5da0a81c5db1db846dc80565cde6a
puneetjakhu1994 <jakhupuneet@gmail.com>
2021-02-04 23:08:30 -0500

    firstcommitmodified_main_news_faqs_forgotpsw

---
+++ app/src/main/java/com/covidinformation/activities/DetailedNewsActivity.java
@@ -0,0 +1,28 @@
+package com.covidinformation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.covidinformation.R;
+
+public class DetailedNewsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_news);
+
+
+
+            }
+}
