fb76b8e77ec6ee9afd404aa89718082ffc0976d7
Srinivas Krishna <srinivaskrishna97@gmail.com>
2021-02-04 21:59:38 -0500

    toast in the main activity

--- app/src/main/java/com/covidinformation/activities/MapActivity.java
+++ app/src/main/java/com/covidinformation/activities/MapActivity.java
@@ -3,6 +3,7 @@ package com.covidinformation.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.covidinformation.R;
 
@@ -12,5 +13,10 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+
+        Toast.makeText(this, "Toast created", Toast.LENGTH_SHORT).show();
+
     }
+
 }
