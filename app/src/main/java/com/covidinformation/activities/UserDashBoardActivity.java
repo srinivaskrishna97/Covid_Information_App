fcf2ba4db1350aa75a32ea1f2aff44b59026ff42
parsh <parshant13rawat@gmail.com>
2021-02-05 12:32:42 -0500

    User Dash board activity is created

---
+++ app/src/main/java/com/covidinformation/activities/UserDashBoardActivity.java
@@ -0,0 +1,33 @@
+package com.covidinformation.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.covidinformation.R;
+import com.google.android.material.navigation.NavigationView;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+public class UserDashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+
+    }
+}
