561490b6930778e7644105baa65db328ca5990b9
ajaysinh9893 <bhati.ajaysinh007@gmail.com>
2021-02-05 00:37:32 -0500

    registration_activity created

---
+++ app/src/main/java/com/covidinformation/activities/RegistrationActivity.java
@@ -0,0 +1,37 @@
+package com.covidinformation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.covidinformation.R;
+import com.covidinformation.api.ApiService;
+import com.covidinformation.api.RetroClient;
+import com.covidinformation.models.ResponseData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_registration);
+
+
+    }
+}
