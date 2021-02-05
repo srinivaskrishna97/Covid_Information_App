cdab399e902b8270813467f464a37bb0b931eed1
Srinivas Krishna <srinivaskrishna97@gmail.com>
2021-02-04 20:43:05 -0500

    Map Activity Created

---
+++ app/src/main/java/com/covidinformation/activities/MapActivity.java
@@ -0,0 +1,16 @@
+package com.covidinformation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.covidinformation.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
