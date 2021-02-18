6fdf97a6f7380ee58d89a9f6bd70a67f33bb306a
ajaysinh9893 <bhati.ajaysinh007@gmail.com>
2021-02-17 22:44:35 -0500

    Admin_login_activity Updated.

--- app/src/main/java/com/covidinformation/activities/AdminLoginActivity.java
+++ app/src/main/java/com/covidinformation/activities/AdminLoginActivity.java
@@ -36,6 +36,71 @@ public class AdminLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_login);
 
 
-       
+        getSupportActionBar().setTitle("Admin Login");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        cirLoginButton=(Button)findViewById(R.id.cirLoginButton);
+        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+
+
+        cirLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if(editTextEmail.getText().toString().isEmpty()){
+                    Toast.makeText(AdminLoginActivity.this, "Please Enter Valid Username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(editTextPassword.getText().toString().isEmpty()){
+                    Toast.makeText(AdminLoginActivity.this, "Please Enter Valid Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                loginFunction();
+
+            }
+        });
+    }
+    public  void loginFunction() {
+        pd= new ProgressDialog(AdminLoginActivity.this);
+        pd.setTitle("Please wait,Data is being submit...");
+        pd.show();
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.adminlogin(editTextEmail.getText().toString(),editTextPassword.getText().toString());
+
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                pd.dismiss();
+                if (response.body().status.equals("true")) {
+                    SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor et=sharedPreferences.edit();
+                    et.putString("user_name",editTextEmail.getText().toString());
+                    et.commit();
+                    startActivity(new Intent(AdminLoginActivity.this, AdminDashBoardActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(AdminLoginActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                pd.dismiss();
+                Toast.makeText(AdminLoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
